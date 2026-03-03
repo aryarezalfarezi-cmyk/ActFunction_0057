9584ca20fe6d277d435809ef07d79d17f3b72c9f
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-03-03 15:46:22 +0700

    Add files via upload

---
+++ pemdas.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+ float p,l;
+
+
+void input() {
+    cout << "Masukkan tinggi: ";
+    cin >> t;
+    cout << "Masukkan lebar: ";
+    cin >> l;
+}
+
+int main() {
+    cout << "Masukkan tinggi: ";
+    cin >> t;
+    cout << "lebar: ";
+    cin >> l;
+
+    float luaspersegi = t * l;
+
+    return 0;
+}
+
+void output() {
+    cout << "Luas persegi: " << luaspersegi << endl;
+}
+
+int main() {
+    input();
+    output();
+    return 0;
+    count << "hasilnya = " << luaspersegi << endl;
+}
